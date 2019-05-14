fede5097a4c790d3f8ea8beabfb85d9765c2c750
kuroemon2509 <yokobang99@gmail.com>
2019-05-14 20:51:52 +0700

    light up led

---
+++ 01_led/light_up_led.py
@@ -0,0 +1,27 @@
+import time
+
+import RPi.GPIO as GPIO
+
+# use BCM GPIO numbering
+GPIO.setmode(GPIO.BCM)
+
+LED_PIN = 4
+
+GPIO.setup(LED_PIN, GPIO.OUT)
+
+try:
+    while True:
+        action = input('Enter action (high, low, exit):')
+
+        if action == 'high':
+            GPIO.output(LED_PIN, GPIO.HIGH)
+        elif action == 'low':
+            GPIO.output(LED_PIN, GPIO.LOW)
+        elif action == 'exit':
+            GPIO.cleanup()
+            break
+        else:
+            pass
+
+except:
+    GPIO.cleanup()
