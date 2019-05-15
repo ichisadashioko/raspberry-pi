d586095e5c44acc6f95b191d534ac05d979eff1e
kuroemon2509 <yokobang99@gmail.com>
2019-05-15 12:06:58 +0700

    control led with pwm

---
+++ 01_led/pwm_led.py
@@ -0,0 +1,24 @@
+import time
+import traceback
+
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+
+LED_PIN = 4
+
+p = GPIO.PWM(LED_PIN, 50)
+
+p.start(0)
+
+try:
+    while True:
+        for i in range(100):
+            p.ChangeDutyCycle(i)
+            time.sleep(0.02)
+        for i in range(100):
+            p.ChangeDutyCycle(100 - i)
+            time.sleep(0.02)
+except:
+    p.stop()
+    GPIO.cleanup()
+    traceback.print_exc()
